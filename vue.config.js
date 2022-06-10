6d7f3972044e85a705a8477e46d6ddf277330040
liuzhidi <462311233@qq.com>
2022-06-10 21:48:45 +0800

    less自动化导入公用变量和混入

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require('path')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  pluginOptions: {
+    //自动给每个组件引入公共样式
+    'style-resources-loader': {
+      preProcessor: 'less',
+      //填写需要引入样式的文件地址
+      patterns: [
+        path.join(__dirname, './src/assets/styles/variables.less'),
+        path.join(__dirname, './src/assets/styles/mixins.less')
+      ]
+    }
+  }
 })
