92449c356c77e338d9874bcaa8fdab236eae40ea
liuzhidi <462311233@qq.com>
2022-06-13 00:11:52 +0800

    商品分类接口

--- src/api/category.js
+++ src/api/category.js
@@ -1,5 +1,4 @@
-//二级分类请求接口
 import request from '@/utils/request.js'
 
-//获取所有分类,拿到一级二级和对应商品的数据
+//获取所有分类,拿到一级二级和对应商品的数据覆盖初始数据
 export const findAllCategory = () => request('/home/category/head', 'get')
