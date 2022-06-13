b16035c80dcabf02c02036d893c0055b2896ef77
liuzhidi <462311233@qq.com>
2022-06-13 23:40:13 +0800

    挂载全局自定义组件

--- src/components/library/index.js
+++ src/components/library/index.js
@@ -3,12 +3,15 @@
 // vue2.0插件写法要素：导出一个对象，有install函数，默认传入了Vue构造函数，Vue基础之上扩展
 // vue3.0插件写法要素：导出一个对象，有install函数，默认传入了app应用实例，app基础之上扩展
 
-import XtxSkeleton from './XtxSkeleton.vue'
-
+import XtxSkeleton from './xtx-skeleton.vue'
+import XtxCarousel from './xtx-carousel.vue'
+import XtxMore from './xtx-more.vue'
 export default {
   install(app) {
     // 在app上进行扩展，app提供 component directive 函数
     // 如果要挂载原型 app.config.globalProperties 方式
     app.component(XtxSkeleton.name, XtxSkeleton)
+    app.component(XtxCarousel.name, XtxCarousel)
+    app.component(XtxMore.name, XtxMore)
   }
 }
