c5468fae600a58f2ebc32511c2c27cfb85c3e17f
liuzhidi <462311233@qq.com>
2022-06-13 23:38:10 +0800

    首页数据请求接口

--- src/api/home.js
+++ src/api/home.js
@@ -1,5 +1,18 @@
+//首页的接口封装
 import request from '@/utils/request.js'
 //品牌分类接口，个数6个
 export const findBrand = (limit = 6) => {
   return request('/home/brand', 'get', { limit })
+}
+//轮播图数据接口
+export const findBanner = () => {
+  return request('/home/banner', 'get')
+}
+//新鲜好物接口
+export const findNew = () => {
+  return request('home/new', 'get')
+}
+//人气推荐接口
+export const findHot = () => {
+  return request('home/hot', 'get')
 }
