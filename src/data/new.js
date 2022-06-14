af5454cf0b676cf37183c1f46a04b570a81453c4
liuzhidi <462311233@qq.com>
2022-06-15 01:24:14 +0800

    数据本地化服务器失效

---
+++ src/data/new.js
@@ -0,0 +1,45 @@
+export const data = {
+  msg: '操作成功',
+  result: [
+    {
+      id: '4000443',
+      name: 'KENROLL男女简洁多彩一片式室外拖',
+      desc: '多彩多色，夏日出行防护',
+      price: '99.00',
+      picture:
+        'https://yanxuan-item.nosdn.127.net/52fdcb0012a0939fc4d92c1e04fccda8.jpg',
+      discount: null,
+      orderNum: 42
+    },
+    {
+      id: '3990979',
+      name: '免浆黑鱼片，酸菜鱼350g',
+      desc: '酸中带辣，开胃爽口酸菜鱼',
+      price: '15.80',
+      picture:
+        'https://yanxuan-item.nosdn.127.net/6d8de4504bdb9c1ad263e6b5b9a18ab2.jpg',
+      discount: null,
+      orderNum: 119
+    },
+    {
+      id: '3993562',
+      name: '室内外两穿船型加厚软弹拖鞋',
+      desc: '重新定义拖鞋，时时刻刻都不将就',
+      price: '29.90',
+      picture:
+        'https://yanxuan-item.nosdn.127.net/7ce1695c9540786d0a8bb302f40fddd3.png',
+      discount: null,
+      orderNum: 41
+    },
+    {
+      id: '3990169',
+      name: '清爽夏日，双层纱宝宝连体衣/居家服套装',
+      desc: '夏日也凉爽，A类无荧光，59-110cm',
+      price: '69.00',
+      picture:
+        'https://yanxuan-item.nosdn.127.net/7f4d7114f16d2996b31382dbf018b0a0.png',
+      discount: null,
+      orderNum: 58
+    }
+  ]
+}
--- src/views/home/components/home-new.vue
+++ src/views/home/components/home-new.vue
@@ -21,7 +21,8 @@
 import HomePanel from '@/views/home/components/home-panel.vue'
 import HomeSkeleton from './home-skeleton.vue'
 import { ref } from 'vue-demi'
-import { findNew } from '@/api/home'
+// import { findNew } from '@/api/home'
+import { data } from '@/data/new.js'
 export default {
   name: 'HomeNew',
   components: {
@@ -30,10 +31,10 @@ export default {
   },
   setup() {
     //获取商品数据
-    const goods = ref([])
-    findNew().then((data) => {
-      goods.value = data.result
-    })
+    const goods = ref(data.result)
+    // findNew().then((data) => {
+    //   goods.value = data.result
+    // })
     return {
       goods
     }
