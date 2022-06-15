6f0fb8208789436759b8a24f655a68d569c842ba
liuzhidi <462311233@qq.com>
2022-06-15 19:10:09 +0800

    一级分类页面

--- src/views/category/index.vue
+++ src/views/category/index.vue
@@ -1,9 +1,22 @@
 <template>
-  <div>1</div>
+  <div class="container">
+    <!-- 面包屑 -->
+    <XtxBread>
+      <XtxBreadItem to="/">首页</XtxBreadItem>
+      <XtxBreadItem to="/category/1005000">电器</XtxBreadItem>
+      <XtxBreadItem>空调</XtxBreadItem>
+    </XtxBread>
+    <!--  -->
+    <!--  -->
+    <!--  -->
+  </div>
 </template>
 
 <script>
+import xtxBread from '@/components/library/xtx-bread.vue'
+import XtxBreadItem from '@/components/library/xtx-bread-item.vue'
 export default {
+  components: { xtxBread, XtxBreadItem },
   name: ''
 }
 </script>
