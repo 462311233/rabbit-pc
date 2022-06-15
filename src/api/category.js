8bd144e0d5890f54479d117e2bfb80c61565e50c
liuzhidi <462311233@qq.com>
2022-06-15 23:42:27 +0800

    分类数据接口

--- src/api/category.js
+++ src/api/category.js
@@ -1,4 +1,10 @@
 import request from '@/utils/request.js'
 
 //获取所有分类,拿到一级二级和对应商品的数据覆盖初始数据
-export const findAllCategory = () => request('/home/category/head', 'get')
+export const findAllCategory = () => {
+  return request('/home/category/head', 'get')
+}
+//获取一级类目
+export const findTopCategory = (id) => {
+  return request('/category', 'get', { id })
+}
