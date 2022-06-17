308d2f461c1cfbfde13ebc734fe95159f9561345
liuzhidi <462311233@qq.com>
2022-06-17 23:27:19 +0800

    二级分类筛选组件

--- src/views/category/components/sub-filter.vue
+++ src/views/category/components/sub-filter.vue
@@ -1,57 +1,103 @@
 <template>
-  <!-- 筛选区 -->
-  <div class="sub-filter" v-if="filterData">
+  <div class="sub-filter" v-if="filterData && !filterLoading">
     <div class="item">
       <div class="head">品牌：</div>
       <div class="body">
         <a
+          :class="{ active: filterData.selectedBrand === brand.id }"
           href="javascript:;"
-          v-for="item in filterData.brands"
-          :key="item.id"
-          >{{ item.name }}</a
+          v-for="brand in filterData.brands"
+          :key="brand.id"
+          >{{ brand.name }}</a
         >
       </div>
     </div>
-    <div class="item" v-for="item in filterData.saleProperties" :key="item.id">
-      <div class="head">{{ item.name }}：</div>
+    <div class="item" v-for="p in filterData.saleProperties" :key="p.id">
+      <div class="head">{{ p.name }}：</div>
       <div class="body">
-        <a href="javascript:;" v-for="prop in item.properties" :key="prop.id">{{
-          prop.name
-        }}</a>
+        <a
+          :class="{ active: p.selectedProp === attr.id }"
+          href="javascript:;"
+          v-for="attr in p.properties"
+          :key="attr.id"
+          >{{ attr.name }}</a
+        >
       </div>
     </div>
   </div>
+  <div v-else class="sub-filter">
+    <XtxSkeleton class="item" width="800px" height="40px" />
+    <XtxSkeleton class="item" width="800px" height="40px" />
+    <XtxSkeleton class="item" width="600px" height="40px" />
+    <XtxSkeleton class="item" width="600px" height="40px" />
+    <XtxSkeleton class="item" width="600px" height="40px" />
+  </div>
 </template>
 <script>
-import { ref, watch } from 'vue-demi'
-import { useRoute } from 'vue-router'
 import { findSubCategoryFilter } from '@/api/category'
+import { useRoute } from 'vue-router'
+import { ref, watch } from 'vue'
 export default {
   name: 'SubFilter',
-  setup() {
+  setup(props, { emit }) {
+    // 1. 获取数据
+    // 2. 数据中需要全部选中，需要预览将来点击激活功能。默认选中全部
+    // 3. 完成渲染
     const route = useRoute()
-    //创建筛选数据
     const filterData = ref(null)
-    //监听路由变化，获取新的筛选数据
+    const filterLoading = ref(false)
+    // 4. 分类发生变化的时候需要重新获取筛选数据，需要使用侦听器
     watch(
       () => route.params.id,
-      (newValue) => {
-        //如果变化后处在二级路由下
-        if (newValue && route.path === `/category/sub/${newValue}`) {
-          //发送请求
-          findSubCategoryFilter(newValue).then((data) => {
-            //可选条件加上‘全部’
-            data.result.brands.unshift({ id: null, name: '全部' })
-            data.result.saleProperties.forEach((item) => {
-              item.properties.unshift({ id: null, name: '全部' })
+      (newVal) => {
+        // 当你从二级分类去顶级分类也会拿到ID，不能去加载数据因为它不是二级分类的ID
+        if (newVal && route.path === '/category/sub/' + newVal) {
+          filterLoading.value = true
+          newVal &&
+            findSubCategoryFilter(route.params.id).then(({ result }) => {
+              // 品牌全部
+              result.selectedBrand = null
+              result.brands.unshift({ id: null, name: '全部' })
+              // 销售属性全部
+              result.saleProperties.forEach((p) => {
+                p.selectedProp = null
+                p.properties.unshift({ id: null, name: '全部' })
+              })
+              filterData.value = result
+              filterLoading.value = false
             })
-            filterData.value = data.result
-          })
         }
       },
       { immediate: true }
     )
-    return { filterData }
+    // 获取筛选参数
+    const getFilterParams = () => {
+      const filterParams = {}
+      const attrs = []
+      filterParams.brandId = filterData.value.selectedBrand
+      filterData.value.saleProperties.forEach((p) => {
+        const attr = p.properties.find((attr) => attr.id === p.selectedProp)
+        if (attr && attr.id !== undefined) {
+          attrs.push({ groupName: p.name, propertyName: attr.name })
+        }
+      })
+      if (attrs.length) filterParams.attrs = attrs
+      return filterParams
+    }
+
+    // 选择品牌
+    const changeBrand = (brandId) => {
+      if (filterData.value.selectedBrand === brandId) return
+      filterData.value.selectedBrand = brandId
+      emit('filter-change', getFilterParams())
+    }
+    // 选中属性
+    const changeAttr = (p, attrId) => {
+      if (p.selectedProp === attrId) return
+      p.selectedProp = attrId
+      emit('filter-change', getFilterParams())
+    }
+    return { filterData, filterLoading, changeBrand, changeAttr }
   }
 }
 </script>
@@ -80,5 +126,8 @@ export default {
       }
     }
   }
+}
+.xtx-skeleton {
+  padding: 10px 0;
 }
 </style>
