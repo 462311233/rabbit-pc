2857dfe90b906434c85301bda7b69b8f68a26784
liuzhidi <462311233@qq.com>
2022-06-22 23:39:03 +0800

    商品详情数据接口

--- src/api/product.js
+++ src/api/product.js
@@ -3,3 +3,45 @@ import request from '@/utils/request.js'
 export const findGoods = (id) => {
   return request('/goods', 'get', { id })
 }
+
+/**
+ * 获取商品同类推荐-未传入ID为猜喜欢
+ * @param {String} id - 商品ID
+ * @param {Number} limit - 获取条数
+ */
+export const findRelevantGoods = (id, limit = 16) => {
+  return request('/goods/relevant', 'get', { id, limit })
+}
+
+/**
+ * 获取热榜商品
+ * @param {Number} type - 1代表24小时热销榜 2代表周热销榜 3代表总热销榜
+ * @param {Number} limit - 获取个数
+ */
+export const findHotGoods = ({ id, type, limit = 3 }) => {
+  return request('/goods/hot', 'get', { id, type, limit })
+}
+
+/**
+ * 获取商品的评价统计信息
+ * @param {String} id - 商品ID
+ */
+export const findCommentInfoByGoods = (id) => {
+  return request(
+    `https://mock.boxuegu.com/mock/1175/goods/${id}/evaluate`,
+    'get'
+  )
+}
+// https://mock.boxuegu.com/mock/1175/goods/${id}/evaluate
+
+/**
+ * 获取商品的评价列表
+ * @param {String} id - 商品ID
+ */
+export const findCommentList = (id, params) => {
+  return request(
+    `https://mock.boxuegu.com/mock/1175/goods/${id}/evaluate/page`,
+    'get',
+    params
+  )
+}
