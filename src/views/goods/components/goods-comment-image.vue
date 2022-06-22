9a8d31006bb3f691e557b86f96ee77cba6594cb9
liuzhidi <462311233@qq.com>
2022-06-22 21:21:00 +0800

    评论图片

---
+++ src/views/goods/components/goods-comment-image.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="goods-comment-image">
+    <div class="list">
+      <a href="javascript:;" v-for="item in pictures" :key="item">
+        <img :src="item" alt="" />
+      </a>
+    </div>
+    <div class="preview" v-if="currImage">
+      <img :src="currImage" alt="" />
+      <i @click="currImage = null" class="iconfont icon-close-new"></i>
+    </div>
+  </div>
+</template>
+<script>
+import { ref } from 'vue'
+export default {
+  name: 'GoodsCommentImage',
+  props: {
+    pictures: {
+      type: Array,
+      default: () => []
+    }
+  },
+  setup() {
+    const currImage = ref(null)
+    return { currImage }
+  }
+}
+</script>
+<style scoped lang="less">
+.goods-comment-image {
+  .list {
+    display: flex;
+    flex-wrap: wrap;
+    margin-top: 10px;
+    a {
+      width: 120px;
+      height: 120px;
+      border: 1px solid #e4e4e4;
+      margin-right: 20px;
+      margin-bottom: 10px;
+      img {
+        width: 100%;
+        height: 100%;
+        object-fit: contain;
+      }
+      &.active {
+        border-color: @xtxColor;
+      }
+    }
+  }
+}
+</style>
