9cda1172ce66173b454fb0eee1e61139d385e568
liuzhidi <462311233@qq.com>
2022-06-25 01:06:44 +0800

    路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,7 @@ const TopCategory = () => import('@/views/category/index.vue')
 const SubCategory = () => import('@/views/category/sub.vue')
 //商品详情
 const Goods = () => import('@/views/goods/index.vue')
+const Login = () => import('@/views/login/index.vue')
 
 const routes = [
   //一级路由布局容器
@@ -20,7 +21,8 @@ const routes = [
       { path: '/category/sub/:id', component: SubCategory },
       { path: '/product/:id', component: Goods }
     ]
-  }
+  },
+  { path: '/login', component: Login }
 ]
 const router = createRouter({
   history: createWebHashHistory(),
